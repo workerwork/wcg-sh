6751a1b7bb85897229c4d28233ca80f791f4f7a1
Gogs <gogs@fake.local>
2018-05-02 12:25:56 +0800

    入口脚本

---
+++ startAll.sh
@@ -0,0 +1,37 @@
+#!/bin/bash -
+#########################################################################################
+# startAll.sh
+# 启动所有进程
+# version:3.0
+# update:20180502
+#########################################################################################
+[ /root/eGW/Config.sh/parameters.conf ] && source /root/eGW/Config.sh/parameters.conf
+CUR_DIR=/root/eGW/Config.sh
+
+#启动redis nginx ipsec
+source $CUR_DIR/init.sh && init
+#sleep 2
+
+#配置iptables
+source $CUR_DIR/iptables.sh && config_iptables
+
+#keepalived
+source $CUR_DIR/keepalived.sh && keepalived
+
+#配置eGW
+source $CUR_DIR/egw.sh && egw
+
+#启动各进程
+source $CUR_DIR/process.sh && process
+
+#启动watchdog
+function watchdog() {
+    while :
+    do
+        source $CUR_DIR/watchdog.sh
+        watchdog_all && wait
+        sleep 1
+    done
+}
+watchdog &
+
