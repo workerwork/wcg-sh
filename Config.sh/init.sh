25eff704851405ae28ffd6d6e33081e49005324e
Gogs <gogs@fake.local>
2018-05-03 18:21:44 +0800

    修正文件夹创建条件

--- Config.sh/init.sh
+++ Config.sh/init.sh
@@ -6,17 +6,17 @@
 # update:20180503
 #########################################################################################
 function init_fold() {
-    [ ! -f /root/eGW/CDR/cdrDat ] && mkdir -p /root/eGW/CDR/cdrDat
-    [ ! -f /root/eGW/ImsiFiles ] && mkdir -p /root/eGW/ImsiFiles
-    [ ! -f /root/eGW/Logs/history ] && mkdir -p /root/eGW/Logs/history
-    [ ! -f /root/eGW/Logs/keepalived ] && mkdir -p /root/eGW/Logs/keepalived
-    [ ! -f /root/eGW/Logs/ltegwd ] && mkdir -p /root/eGW/Logs/ltegwd
-    [ ! -f /root/eGW/Logs/tcpdump ] && mkdir -p /root/eGW/Logs/tcpdump
-    [ ! -f /root/eGW/Logs/vtysh ] && mkdir -p /root/eGW/Logs/vtysh
-    [ ! -f /root/eGW/Logs/watchdog ] && mkdir -p /root/eGW/Logs/watchdog
-    [ ! -f /root/eGW/Logs/omcapi/manage ] && mkdir -p /root/eGW/Logs/omcapi/manage
-    [ ! -f /root/eGW/Logs/omcapi/monitor ] && mkdir -p /root/eGW/Logs/omcapi/monitor
-    [ ! -f /root/eGW/Logs/omcapi/report ] && mkdir -p /root/eGW/Logs/omcapi/report
+    [ ! -d /root/eGW/CDR/cdrDat ] && mkdir -p /root/eGW/CDR/cdrDat
+    [ ! -d /root/eGW/ImsiFiles ] && mkdir -p /root/eGW/ImsiFiles
+    [ ! -d /root/eGW/Logs/history ] && mkdir -p /root/eGW/Logs/history
+    [ ! -d /root/eGW/Logs/keepalived ] && mkdir -p /root/eGW/Logs/keepalived
+    [ ! -d /root/eGW/Logs/ltegwd ] && mkdir -p /root/eGW/Logs/ltegwd
+    [ ! -d /root/eGW/Logs/tcpdump ] && mkdir -p /root/eGW/Logs/tcpdump
+    [ ! -d /root/eGW/Logs/vtysh ] && mkdir -p /root/eGW/Logs/vtysh
+    [ ! -d /root/eGW/Logs/watchdog ] && mkdir -p /root/eGW/Logs/watchdog
+    [ ! -d /root/eGW/Logs/omcapi/manage ] && mkdir -p /root/eGW/Logs/omcapi/manage
+    [ ! -d /root/eGW/Logs/omcapi/monitor ] && mkdir -p /root/eGW/Logs/omcapi/monitor
+    [ ! -d /root/eGW/Logs/omcapi/report ] && mkdir -p /root/eGW/Logs/omcapi/report
 }
 
 function init_net() {
