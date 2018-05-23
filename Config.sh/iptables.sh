6cb5093d1236435807799399872ce46033def2f0
Gogs <gogs@fake.local>
2018-05-23 15:15:18 +0800

    去掉中文注释

--- Config.sh/iptables.sh
+++ Config.sh/iptables.sh
@@ -1,25 +1,23 @@
 #!/bin/bash -
-
 #########################################################################################
 # iptables.sh
-# 防火墙程序,定义iptables规则,对公网口进行过滤
-# version:3.0
-# update:20180502
+# version:3.1
+# update:20180523
 #########################################################################################
 function init_iptables() {
-    iptables -F         #删除filter表规则 
-    iptables -F -t nat  #删除nat表规则
-    iptables -X         #删除filter表用户定义规则 
-    iptables -X -t nat	#删除nat表用户定义规则
-    iptables -Z         #清空filter表计数器
-    iptables -Z -t nat  #清空nat表计数器
+    iptables -F          
+    iptables -F -t nat  
+    iptables -X         
+    iptables -X -t nat	
+    iptables -Z         
+    iptables -Z -t nat  
 }
 
 
 function set_default_policy_iptables() {
-    iptables -P INPUT ACCEPT      #允许所有包进入
-    iptables -P OUTPUT ACCEPT     #允许所有包出去
-    iptables -P FORWARD ACCEPT    #允许所有包转发
+    iptables -P INPUT ACCEPT      
+    iptables -P OUTPUT ACCEPT     
+    iptables -P FORWARD ACCEPT    
 }
 
 function set_firewalld_iptables() {
@@ -32,16 +30,16 @@ function set_firewalld_iptables() {
     if [[ $iptables_switch -eq 1 ]];then
         init_iptables
         set_default_policy_iptables
-        iptables -A INPUT -p udp --sport 53 -j ACCEPT										#允许DNS
-        iptables -A INPUT -p udp --dport 53 -j ACCEPT										#允许DNS
-        iptables -A INPUT -p tcp --dport 50683 -j ACCEPT									#允许SSH登录
-        iptables -A INPUT -p udp --dport 500 -j ACCEPT										#允许IPSEC握手
-        iptables -A INPUT -p udp --dport 4500 -j ACCEPT										#允许IPSEC隧道包
-        iptables -A INPUT -p sctp --dport 36412 -j ACCEPT									#允许SCTP包
-        iptables -A INPUT -m state --state RELATED,ESTABLISHED -j ACCEPT    				#允许已经建链的包和响应包
-        iptables -A INPUT -p icmp -j ACCEPT													#允许ICMP包
-        iptables -A INPUT -p esp -j ACCEPT                                  				#允许ESP包
-        [ $iptables_interface ] && iptables -A INPUT -p all -i ${iptables_interface} -j DROP	#丢弃指定端口包
+        iptables -A INPUT -p udp --sport 53 -j ACCEPT										
+        iptables -A INPUT -p udp --dport 53 -j ACCEPT										
+        iptables -A INPUT -p tcp --dport 50683 -j ACCEPT									
+        iptables -A INPUT -p udp --dport 500 -j ACCEPT										
+        iptables -A INPUT -p udp --dport 4500 -j ACCEPT										
+        iptables -A INPUT -p sctp --dport 36412 -j ACCEPT									
+        iptables -A INPUT -m state --state RELATED,ESTABLISHED -j ACCEPT    				
+        iptables -A INPUT -p icmp -j ACCEPT													
+        iptables -A INPUT -p esp -j ACCEPT                                  				
+        [ $iptables_interface ] && iptables -A INPUT -p all -i ${iptables_interface} -j DROP	
     fi
 }
 
@@ -50,4 +48,3 @@ function config_iptables() {
     set_firewalld_iptables
 }
 
-#config_iptables
