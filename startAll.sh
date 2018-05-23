ca82e1c13222eefdd6d61d2cf4de3e874efeea8f
Gogs <gogs@fake.local>
2018-05-23 15:05:15 +0800

    修改startAll.sh，中文注释替换成英文注释

--- startAll.sh
+++ startAll.sh
@@ -1,31 +1,30 @@
 #!/bin/bash -
 #########################################################################################
 # startAll.sh
-# 启动所有进程
-# version:3.0
-# update:20180502
+# version:3.1
+# update:20180523
 #########################################################################################
 [ /root/eGW/Config.sh/parameters.conf ] && source /root/eGW/Config.sh/parameters.conf
 CUR_DIR=/root/eGW/Config.sh
 
-#启动redis nginx ipsec
+#init the redis nginx ipsec
 source $CUR_DIR/init.sh && init
 #sleep 2
 
-#配置iptables
+#configure iptables
 source $CUR_DIR/iptables.sh && config_iptables
 
-#keepalived
+#configure keepalived
 source $CUR_DIR/keepalived.sh && keepalived
 
-#启动各进程
+#start process
 source $CUR_DIR/process.sh && process
 sleep 1
 
-#配置eGW
+#configure eGW
 source $CUR_DIR/egw.sh && egw 
 
-#启动watchdog
+#start watchdog
 function watchdog() {
     while :
     do
