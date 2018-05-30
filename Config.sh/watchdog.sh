1f02fc4831bf2990bd982225f3a7ec66bd774272
Gogs <gogs@fake.local>
2018-05-30 09:56:46 +0800

    调整格式

--- Config.sh/watchdog.sh
+++ Config.sh/watchdog.sh
@@ -16,17 +16,17 @@ function ps() {
     export -f ipsec_test
 
     local watch="/root/eGW/Config.sh/watchdog_ps.sh"
-	[[ -z $(ps -ef | grep "ps_ltegwd watchdog_ltegwd_timer$") ]] && \
+    [[ -z $(ps -ef | grep "ps_ltegwd watchdog_ltegwd_timer$") ]] && \
     $watch ps_ltegwd watchdog_ltegwd_timer &
-	[[ -z $(ps -ef | grep "ps_egw_manage watchdog_manage_timer$") ]] && \
+    [[ -z $(ps -ef | grep "ps_egw_manage watchdog_manage_timer$") ]] && \
     $watch ps_egw_manage watchdog_manage_timer &
-	[[ -z $(ps -ef | grep "ps_egw_report watchdog_report_timer$") ]] && \
+    [[ -z $(ps -ef | grep "ps_egw_report watchdog_report_timer$") ]] && \
     $watch ps_egw_report watchdog_report_timer &
-	[[ -z $(ps -ef | grep "ps_egw_monitor watchdog_monitor_timer$") ]] && \
+    [[ -z $(ps -ef | grep "ps_egw_monitor watchdog_monitor_timer$") ]] && \
     $watch ps_egw_monitor watchdog_monitor_timer &
-	[[ -z $(ps -ef | grep "ps_egw_manage_logger watchdog_manage_logger_timer$") ]] && \
+    [[ -z $(ps -ef | grep "ps_egw_manage_logger watchdog_manage_logger_timer$") ]] && \
     $watch ps_egw_manage_logger watchdog_manage_logger_timer &
-	[[ -z $(ps -ef | grep "ipsec_test watchdog_ipsec_test_timer$") ]] && \
+    [[ -z $(ps -ef | grep "ipsec_test watchdog_ipsec_test_timer$") ]] && \
     $watch ipsec_test watchdog_ipsec_test_timer &
 }
 
@@ -38,7 +38,7 @@ function cdr() {
     export -f cdr_del
 
     local watch="/root/eGW/Config.sh/watchdog_cdr.sh"
-	[[ -z $(ps -ef | grep "cdr_all watchdog_cdr_timer watchdog_cdr_number$") ]] && \
+    [[ -z $(ps -ef | grep "cdr_all watchdog_cdr_timer watchdog_cdr_number$") ]] && \
     $watch cdr_all watchdog_cdr_timer watchdog_cdr_number &
 }
 
@@ -54,21 +54,21 @@ function log() {
     export -f vtysh_log
     
     local watch="/root/eGW/Config.sh/watchdog_log.sh"
-	[[ -z $(ps -ef | grep "ps_log watchdog_ps_log_timer watchdog_ps_log_number$") ]] && \
+    [[ -z $(ps -ef | grep "ps_log watchdog_ps_log_timer watchdog_ps_log_number$") ]] && \
     $watch ps_log watchdog_ps_log_timer watchdog_ps_log_number &
-	[[ -z $(ps -ef | grep "history_log watchdog_history_log_timer watchdog_history_log_number$") ]] && \
+    [[ -z $(ps -ef | grep "history_log watchdog_history_log_timer watchdog_history_log_number$") ]] && \
     $watch history_log watchdog_history_log_timer watchdog_history_log_number &
-	[[ -z $(ps -ef | grep "keepalived_log watchdog_keepalived_log_timer watchdog_keepalived_log_number$") ]] && \
+    [[ -z $(ps -ef | grep "keepalived_log watchdog_keepalived_log_timer watchdog_keepalived_log_number$") ]] && \
     $watch keepalived_log watchdog_keepalived_log_timer watchdog_keepalived_log_number &
-	[[ -z $(ps -ef | grep "ltegwd_log watchdog_ltegwd_log_timer watchdog_ltegwd_log_number$") ]] && \
+    [[ -z $(ps -ef | grep "ltegwd_log watchdog_ltegwd_log_timer watchdog_ltegwd_log_number$") ]] && \
     $watch ltegwd_log watchdog_ltegwd_log_timer watchdog_ltegwd_log_number &
-	[[ -z $(ps -ef | grep "manage_log watchdog_manage_log_timer watchdog_manage_log_number$") ]] && \
+    [[ -z $(ps -ef | grep "manage_log watchdog_manage_log_timer watchdog_manage_log_number$") ]] && \
     $watch manage_log watchdog_manage_log_timer watchdog_manage_log_number &
-	[[ -z $(ps -ef | grep "report_log watchdog_report_log_timer watchdog_report_log_number$") ]] && \
+    [[ -z $(ps -ef | grep "report_log watchdog_report_log_timer watchdog_report_log_number$") ]] && \
     $watch report_log watchdog_report_log_timer watchdog_report_log_number &
-	[[ -z $(ps -ef | grep "monitor_log watchdog_monitor_log_timer watchdog_monitor_log_number$") ]] && \
+    [[ -z $(ps -ef | grep "monitor_log watchdog_monitor_log_timer watchdog_monitor_log_number$") ]] && \
     $watch monitor_log watchdog_monitor_log_timer watchdog_monitor_log_number &
-	[[ -z $(ps -ef | grep "vtysh_log watchdog_vtysh_log_timer wvatchdog_vtysh_log_number$") ]] && \
+    [[ -z $(ps -ef | grep "vtysh_log watchdog_vtysh_log_timer wvatchdog_vtysh_log_number$") ]] && \
     $watch vtysh_log watchdog_vtysh_log_timer watchdog_vtysh_log_number &
 }
 
